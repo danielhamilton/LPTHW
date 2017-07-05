18ef1c6ce2efcb22231d4498fa38d1d8486562cc
danielhamilton <dh@danielhamilton.com>
2017-07-05 01:54:51 -0400

    Adding new examples

--- ex15.py
+++ ex15.py
@@ -1,15 +1,22 @@
+#imports the argv feature from the sys package
 from sys import argv
 
+#the format for this is run the script name and the filename
 script, filename = argv
 
+#opens the filename we specify above
 txt = open(filename)
-    
+
+#prints message with filename, then content of the txt file    
 print "Here's your file %r:" % filename
 print txt.read()
 
-print "Type the filename again: "
-file_again = raw_input("> ")
-    
+#enter the name again
+#print "Type the filename again: "
+file_again = raw_input("Enter the filename again > ")
+
+#open the file again   
 txt_again = open(file_again)
-    
+
+#print the content of the txt again
 print txt_again.read()
